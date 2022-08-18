547cdd1305f88a5eb5b53b6d35d610b72ce4636c
Dor Levi <30637457+dorlevi121@users.noreply.github.com>
2022-08-18 16:52:04 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cye-chat-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cye-chat';
+}
